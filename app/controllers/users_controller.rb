1d1280b8dec43cfdf634245b5e6f4b3d1e4a664b
Jesse Zelaya <jessezelaya1@gmail.com>
2015-07-28 16:57:09 -0700

    added new create and user params actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
   end
+
+  def create
+      @user = User.new
+      if @user.save
+        redirect_to users_path
+      else
+        render 'new'
+      end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email , :password, :password_confirmation)
+  end
+
 end
