23d42be88513b873c3a0c4ce9e83cfdca521ff30
Jesse Zelaya <jessezelaya1@gmail.com>
2015-07-29 14:38:42 -0700

    changed redirect to user index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
 
-      redirect_to root_path, notice: "logged in!"
+      redirect_to users_path, notice: "logged in!"
     else
       flash.now.alert = "invalid login credentials"
       render 'new'
